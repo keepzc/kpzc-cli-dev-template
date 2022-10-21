d7a427586fcaba7c6e7db2c10fc95316b3161a86
mengbingzc <13699200852@163.com>
2022-10-21 18:32:36 +0800

    项目组件模版管理模版

---
+++ kpzc-cli-template-custom-vue3/template/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    vue3 template
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
